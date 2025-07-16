fed9970835e70433e8ac7df396123fdcebefc4db
Ben Milde <bmilde@gmail.com>
2025-07-16 15:01:48 +0200

    added tran script for spm

---
+++ train_sentencepiece.sh
@@ -0,0 +1,4 @@
+# usage: train_sentencepiece.py [-h] --input INPUT [--model_prefix MODEL_PREFIX] [--vocab_size VOCAB_SIZE] [--character_coverage CHARACTER_COVERAGE] [--model_type {unigram,bpe,char,word}] [--min_frequency MIN_FREQUENCY] [--unk_piece UNK_PIECE] [--pad_piece PAD_PIECE] [--bos_piece BOS_PIECE] [--eos_piece EOS_PIECE]
+# train_sentencepiece.py: error: the following arguments are required: --input
+
+python3 train_sentencepiece.py --input data_en_33k_lm_train.txti --vocab_size 1024 --model_type bpe --min_frequency 5
