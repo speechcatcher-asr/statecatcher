c21057a6609dd222ea79e8feb235b634a2d6e335
Ben Milde <bmilde@gmail.com>
2025-07-16 16:14:59 +0200

    default coverage 0.998

--- train_sentencepiece.sh
+++ train_sentencepiece.sh
@@ -1,4 +1,4 @@
 # usage: train_sentencepiece.py [-h] --input INPUT [--model_prefix MODEL_PREFIX] [--vocab_size VOCAB_SIZE] [--character_coverage CHARACTER_COVERAGE] [--model_type {unigram,bpe,char,word}] [--unk_piece UNK_PIECE] [--pad_piece PAD_PIECE] [--bos_piece BOS_PIECE] [--eos_piece EOS_PIECE]
 # train_sentencepiece.py: error: the following arguments are required: --input
 
-python3 train_sentencepiece.py --input data_en_33k_lm_train.txt --vocab_size 1024 --model_type bpe --character_coverage 0.995
+python3 train_sentencepiece.py --input data_en_33k_lm_train.txt --vocab_size 1024 --model_type bpe --character_coverage 0.998
