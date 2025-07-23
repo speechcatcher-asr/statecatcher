95963e16458fcdb2199190e79bb574402dc3534c
Ben Milde <bmilde@gmail.com>
2025-07-23 23:53:10 +0200

    4 layers default for lstm

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python3 train.py --sp-model spm_model.model --frontend mfcc --encoder lstm --mode ctc --embedding-dim -1 --debug
+python3 train.py --sp-model spm_model.model --frontend mfcc --encoder lstm --mode ctc --embedding-dim -1 --num-layers 4 --debug
